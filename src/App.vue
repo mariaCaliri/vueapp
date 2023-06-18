1479b5b60e89b1b5513e061e785e28514f86de9b
CaliriMaria <maria.caliri@tecnasoft.it>
2023-06-18 13:07:16 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+      <learning-resource v-for="res in storedResources" 
+      :key="res.id"
+      :name="res.name"
+      :description="res.description"
+      :link="res.link"
+      ></learning-resource>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+      LearningResource
+  },
+  data() {
+      return {
+          storedResources: [
+              {
+                  id: 1,
+                  name: 'Resource 1',
+                  description: 'Vue documentation',
+                  link: 'https://www.vuejs.org'
+              },
+              {
+                  id: 2,
+                  name: 'Resource 2',
+                  description: 'This is the description for google',
+                  link: 'https://www.google.com'
+              },
+              {
+                  id: 3,
+                  name: 'Resource 3',
+                  description: 'This is the description for resource 3',
+                  link: 'https://www.google.com'
+              }
+
+          ]
+      }
+  },
+  methods: {
+     
+  },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+box-sizing: border-box;
 }
+
+html {
+font-family: 'Roboto', sans-serif;
+}
+
+body {
+margin: 0;
+}
+
 </style>
---
+++ src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,75 @@
+<template>
+    <li >
+        <div>
+            <header>
+                <h3> {{ name }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p> {{ description }}</p>
+        <nav>
+            <a :href="link">View the resource</a>
+        </nav>
+    </li>
+        
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    },
+
+}
+
+
+</script>
+
+<style> li {
+    margin: auto;
+    max-width: 40rem;
+  }
+  
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  h3 {
+    font-size: 1.25rem;
+    margin: 0.5rem 0;
+  }
+  
+  p {
+    margin: 0.5rem 0;
+  }
+  
+  a {
+    text-decoration: none;
+    color: #ce5c00;
+  }
+  
+  a:hover,
+  a:active {
+    color: #c89300;
+  }</style>
